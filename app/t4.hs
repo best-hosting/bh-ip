2c36c656872738b292925294af470f4e3e21d243
sgf <sgf.dma@gmail.com>
2022-02-11 21:16:26 +0300

    save

--- app/t4.hs
+++ app/t4.hs
@@ -1,23 +1,56 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.Map as M
 import Data.Maybe
+import Data.Yaml
+import qualified Data.Text as T
+import qualified Data.ByteString as B
+import qualified Data.Aeson.Types as J
+import qualified Data.Aeson.Encoding as JE
 
 main :: IO ()
-main = print "ura"
+main = showMap testA
 
 newtype MacAddr = MacAddr String
   deriving (Show, Eq, Ord)
 
+instance ToJSON MacAddr where
+    toJSON (MacAddr str) = String (T.pack str)
+
+instance J.ToJSONKey MacAddr where
+    toJSONKey = J.ToJSONKeyText (\(MacAddr str) -> T.pack str) (\(MacAddr str) -> JE.text . T.pack $ str)
+
 newtype Port = Port String
   deriving (Show, Eq, Ord)
+
+instance ToJSON Port where
+    toJSON (Port str) = String (T.pack str)
+
+instance J.ToJSONKey Port where
+    toJSONKey = J.ToJSONKeyText (\(Port str) -> T.pack str) (\(Port str) -> JE.text . T.pack $ str)
 
 data MacData = MacData {macPort :: Port, macServer :: String}
   deriving (Show)
 
+instance ToJSON MacData where
+    toJSON MacData {..} =
+        object $
+          [ "port" .= macPort
+          , "server" .= macServer
+          ]
+
 data PortData = PortData {portMacs :: [MacAddr], portName :: String}
   deriving (Show)
+
+instance ToJSON PortData where
+    toJSON PortData {..} =
+        object
+          [ "macs" .= portMacs
+          , "name" .= portName
+          ]
 
 data GenRef = GenRef {refMac :: Maybe MacAddr, refPort :: Maybe Port}
   deriving (Show)
@@ -26,19 +59,19 @@ type MacMap = M.Map MacAddr MacData
 
 testA :: MacMap
 testA = M.fromList
-    [ (MacAddr "11", MacData {macPort = Port "1", macServer = "bh1"})
-    , (MacAddr "12", MacData {macPort = Port "1", macServer = "bh1"})
-    , (MacAddr "2" , MacData {macPort = Port "1", macServer = "bh2"})
-    , (MacAddr "3" , MacData {macPort = Port "2", macServer = "bh3"})
+    [ (MacAddr "1:1:1", MacData {macPort = Port "sw/1", macServer = "bh1"})
+    , (MacAddr "1:1:2", MacData {macPort = Port "sw/1", macServer = "bh1"})
+    , (MacAddr "2:2:2" , MacData {macPort = Port "sw/1", macServer = "bh2"})
+    , (MacAddr "3:3:3" , MacData {macPort = Port "sw/2", macServer = "bh3"})
     ]
 
 type PortMap = M.Map Port PortData
 
 testB :: PortMap
 testB = M.fromList
-    [ (Port "1", PortData {portMacs = [MacAddr "11", MacAddr "12", MacAddr "2"], portName = "p1"})
-    , (Port "2", PortData {portMacs = [MacAddr "3"], portName = "p2"})
-    , (Port "3", PortData {portMacs = [], portName = "p3"})
+    [ (Port "sw/1", PortData {portMacs = [MacAddr "1:1:1", MacAddr "1:1:2", MacAddr "2:2:2"], portName = "p1"})
+    , (Port "sw/2", PortData {portMacs = [MacAddr "3:3:3"], portName = "p2"})
+    , (Port "sw/3", PortData {portMacs = [], portName = "p3"})
     ]
 
 class Collection ce where
@@ -94,4 +127,36 @@ instance Collection (M.Map Port PortData) where
 
 updateMac :: Maybe MacAddr -> GenRef -> MacMap -> MacMap
 updateMac m r mm = foldr (\x z -> update x r z) mm (buildRef m mm)
+
+updateByMac :: (MacAddr, Maybe Port) -> MacMap -> MacMap
+updateByMac (mac, mport) mm =
+    let [ref0] = buildRef (Just mac) mm
+        ref1 = GenRef {refMac = Just mac, refPort = mport}
+    in  update ref0 ref1 mm
+
+updateByPort :: (Port, [MacAddr]) -> PortMap -> PortMap
+updateByPort (port, newMacs) pm0 =
+    let refs0 = buildRef (Just port) pm0
+        oldMacs = mapMaybe refMac refs0
+    in  refsAdd oldMacs . refsRemove oldMacs $ pm0
+  where
+    refsAdd :: [MacAddr] -> PortMap -> PortMap
+    refsAdd oldMacs pm = foldr
+        (\mac pz ->
+            if mac `notElem` oldMacs
+              then update ref0 GenRef{refMac = Just mac, refPort = Just port} pz
+              else pz
+        ) pm newMacs
+      where ref0 = GenRef{refMac = Nothing, refPort = Just port}
+    refsRemove :: [MacAddr] -> PortMap -> PortMap
+    refsRemove oldMacs pm = foldr
+        (\mac pz ->
+            if mac `notElem` newMacs
+              then update GenRef{refMac = Just mac, refPort = Just port} ref1 pz
+              else pz
+        ) pm oldMacs
+      where ref1 = GenRef {refMac = Nothing, refPort = Just port}
+
+showMap :: (J.ToJSONKey a, ToJSON b) => M.Map a b -> IO ()
+showMap mm = B.putStr (encode mm)
 
