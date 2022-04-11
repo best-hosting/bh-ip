54fff9efdb2a3fe9c6ed5bb9a6bf69a9bc38c0c1
sgf <sgf.dma@gmail.com>
2022-04-11 17:59:43 +0300

    Update name on port

--- app/t4.hs
+++ app/t4.hs
@@ -143,17 +143,20 @@ testA :: MacMap
 testA = M.fromList
     [ (MacAddr "1:1:1", MacData {macPort = pure (Port "sw/1"), macServer = pure "bh1", macState = pure Answering})
     , (MacAddr "1:1:2", MacData {macPort = pure (Port "sw/1"), macServer = pure "bh1", macState = pure Answering})
-    , (MacAddr "2:2:2" , MacData {macPort = pure (Port "sw/1"), macServer = pure "bh2", macState = pure Answering})
+    , (MacAddr "2:2:2" , MacData {macPort = pure (Port "sw/1"), macServer = pure "bh1", macState = pure Answering})
     , (MacAddr "3:3:3" , MacData {macPort = pure (Port "sw/2"), macServer = pure "bh3", macState = pure Answering})
     ]
 
 type PortMap = M.Map Port PortData
 
+-- FIXME: Port may have a list of names.. huh?
+-- FIXME: Define type for all fields to avoid (pure []) and the like
+-- constructs.
 testB :: PortMap
 testB = M.fromList
-    [ (Port "sw/1", PortData {portMacs = pure [MacAddr "1:1:1", MacAddr "1:1:2", MacAddr "2:2:2"], portName = pure "p1", portState = pure Up})
-    , (Port "sw/2", PortData {portMacs = pure [MacAddr "3:3:3"], portName = pure "p2", portState = pure Up})
-    , (Port "sw/3", PortData {portMacs = pure [], portName = pure "p3", portState = pure Up})
+    [ (Port "sw/1", PortData {portMacs = pure [MacAddr "1:1:1", MacAddr "1:1:2", MacAddr "2:2:2"], portName = pure "bh1", portState = pure Up})
+    , (Port "sw/2", PortData {portMacs = pure [MacAddr "3:3:3"], portName = pure "bh3", portState = pure Up})
+    , (Port "sw/3", PortData {portMacs = pure [], portName = pure "", portState = pure Up})
     ]
 
 
@@ -172,6 +175,25 @@ macUpdateRef (MacPortRef oldMac oldPort) (MacPortRef newMac newPort) mm =
     . maybe id (\m ->
         M.adjust
           (\d -> d{macPort = First . setPort Nothing $ getFirst (macPort d)})
+          m)
+      oldMac
+    $ mm
+
+macUpdateRef2 :: NameRef -> NameRef -> MacMap -> MacMap
+macUpdateRef2 (NameRef oldMac oldPort oldName) (NameRef newMac newPort newName) mm =
+    maybe id (\m ->
+        M.insertWith
+          (\_ old ->
+              old{ macPort = First . setPort newPort $ getFirst (macPort old)
+                 , macServer = First newName
+                 })
+        m mempty{macPort = First newPort, macServer = First newName})
+      newMac
+    . maybe id (\m ->
+        M.adjust
+          (\d -> d{ macPort = First Nothing
+                  , macServer = First Nothing
+                  })
           m)
       oldMac
     $ mm
@@ -209,6 +231,38 @@ portUpdateRef (MacPortRef oldMac oldPort) (MacPortRef newMac newPort) pm =
       oldPort
     $ pm
 
+-- This is effectively (setL new . setL mempty) for all fields. Maybe a class
+-- parametrizing /lenses/ over data type may allow to merge all these into one
+-- function.
+portUpdateRef2 :: NameRef -> NameRef -> PortMap -> PortMap
+portUpdateRef2 (NameRef oldMac oldPort oldName) (NameRef newMac newPort newName) pm =
+    maybe id (\p ->
+        M.insertWith
+          (\_ old ->
+              old{ portMacs = pure
+                      . addMac Nothing newMac
+                      $ (fromMaybe [] $ getFirst (portMacs old))
+                 , portName = First newName
+                 })
+        p mempty{portMacs = First $ (: []) <$> newMac, portName = First newName})
+      newPort
+    . maybe id (\p ->
+        M.adjust
+          (\d ->
+              d{portMacs = pure
+                      -- If i trust consistency of references, i should not
+                      -- delete new mac on old port, because that was /not/
+                      -- explicitly requested.
+                      -- . addMac (Just newMac) Nothing
+                      . addMac oldMac Nothing
+                      $ (fromMaybe [] $ getFirst (portMacs d))
+               , portName = mempty
+               }
+          )
+          p)
+      oldPort
+    $ pm
+
 -- This function may be the most generic 'update' function, which should be
 -- instantiated according to some type-class instances.
 addMac :: (Eq a) => Maybe a -> Maybe a -> [a] -> [a]
@@ -223,21 +277,29 @@ updateRef ref (mm, pm) =
     , uncurry portUpdateRef ref pm
     )
 
-updateByMac :: (MacAddr, Maybe Port) -> (MacMap, PortMap) -> (MacMap, PortMap)
-updateByMac (mac, newPort) (mm0, pm0) =
+updateRef2 :: (NameRef, NameRef) -> (MacMap, PortMap) -> (MacMap, PortMap)
+updateRef2 ref (mm, pm) =
+    ( uncurry macUpdateRef2 ref mm
+    , uncurry portUpdateRef2 ref pm
+    )
+
+updateMacPort :: (MacAddr, Maybe Port) -> (MacMap, PortMap) -> (MacMap, PortMap)
+updateMacPort (mac, newPort) (mm0, pm0) =
     let refs =
           ( MacPortRef{refMac = const mac <$> M.lookup mac mm0, refPort = M.lookup mac mm0 >>= getFirst . macPort}
           , MacPortRef{refMac = Just mac, refPort = newPort}
           )
     in  updateRef refs (mm0, pm0)
 
-updateByPort :: (Port, [MacAddr]) -> (MacMap, PortMap) -> (MacMap, PortMap)
-updateByPort (newPort, newMacs) (mm0, pm0) =
-    let oldMacs = fromMaybe [] $ M.lookup newPort pm0 >>= getFirst . portMacs
+updatePortMacs :: (Port, [MacAddr]) -> (MacMap, PortMap) -> (MacMap, PortMap)
+updatePortMacs (port, newMacs) (mm0, pm0) =
+    let oldMacs = fromMaybe [] $ M.lookup port pm0 >>= getFirst . portMacs
 
+        -- For adding port i don't even need to check whether it's already
+        -- there or not, just assume, it's not there.
         refAddPort =
-          ( MacPortRef{refMac = Nothing, refPort = const newPort <$> M.lookup newPort pm0}
-          , MacPortRef{refMac = Nothing, refPort = Just newPort}
+          ( MacPortRef{refMac = Nothing, refPort = Nothing}
+          , MacPortRef{refMac = Nothing, refPort = Just port}
           )
 
         -- For macs, which are no longer present, remove mac-port binding,
@@ -245,32 +307,83 @@ updateByPort (newPort, newMacs) (mm0, pm0) =
         -- different refs for MacMap and PortMap.
         -- FIXME: Do i need this now? 'portUpdateRef' does not delete any
         -- keys.
+        -- If there's no port yet, 'oldMacs' will be empty. So i may safely
+        -- assume, that if 'foldr' run, port was already there.
         (mm1, pm1) =
           foldr
             (\mac (mm, pm) ->
               let refs =
-                    ( MacPortRef {refMac = Just mac, refPort = const newPort <$> M.lookup newPort pm0}
-                    , MacPortRef {refMac = Nothing, refPort = Just newPort}
+                    ( MacPortRef {refMac = Just mac, refPort = Just port}
+                    , MacPortRef {refMac = Nothing , refPort = Just port}
                     )
               in  updateRef refs (mm, pm)
             )
             (mm0, pm0)
             (oldMacs \\ newMacs)
 
-        refsAdd = map (\mac ->
-                      ( MacPortRef {refMac = const mac <$> M.lookup mac mm0, refPort = M.lookup mac mm0 >>= getFirst . macPort}
-                      , MacPortRef {refMac = Just mac, refPort = Just newPort}
-                      )
-                   )
-                   (newMacs \\ oldMacs)
+        refsAdd =
+          foldr
+            (\mac z ->
+                  ( MacPortRef {refMac = const mac <$> M.lookup mac mm0, refPort = M.lookup mac mm0 >>= getFirst . macPort}
+                  , MacPortRef {refMac = Just mac, refPort = Just port}
+                  )
+                  : z
+             )
+             [refAddPort]
+             (newMacs \\ oldMacs)
 
-    in  foldr updateRef (updateRef refAddPort (mm1, pm1)) refsAdd
-
-
+    in  foldr updateRef (mm1, pm1) refsAdd
 
 showMap :: (J.ToJSONKey a, ToJSON b) => M.Map a b -> IO ()
 showMap mm = B.putStr (encode mm)
 
 showAll :: (MacMap, PortMap) -> IO ()
 showAll (mm, pm) = showMap mm >> showMap pm
+
+data NameRef = NameRef  { nrMac  :: Maybe MacAddr
+                        , nrPort :: Maybe Port
+                        , nrName :: Maybe String
+                        }
+  deriving (Show)
+
+updatePortName :: (Port, String) -> (MacMap, PortMap) -> (MacMap, PortMap)
+updatePortName (port, newName) (mm0, pm0) =
+    let oldMacs = fromMaybe [] $ M.lookup port pm0 >>= getFirst . portMacs
+        oldName = M.lookup port pm0 >>= getFirst . portName
+        (mm1, pm1) =
+          foldr
+            (\mac (mm, pm) ->
+              let refs =
+                    ( NameRef
+                        { nrMac = Just mac
+                        , nrPort = Just port
+                        , nrName = oldName
+                        }
+                    , NameRef
+                        { nrMac  = Just mac
+                        , nrPort = Nothing
+                        , nrName = oldName
+                        }
+                    )
+              in  updateRef2 refs (mm, pm)
+            )
+            (mm0, pm0)
+            oldMacs
+        refAddName =
+            ( NameRef
+                { nrMac = Nothing
+                , nrPort = Just port
+                , nrName = oldName
+                }
+            , NameRef
+                { nrMac = Nothing
+                , nrPort = Just port
+                , nrName = Just newName
+                }
+            )
+    in updateRef2 refAddName (mm1, pm1)
+
+{-updateMacName :: (MacAddr, String) -> (MacMap, PortMap) -> (MacMap, PortMap)
+updateMacName (mac, newName) (mm0, pm0) =
+    let ...-}
 
